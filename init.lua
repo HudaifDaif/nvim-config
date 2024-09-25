b6621b95a702297c1b34d049e44492b5c26f5332
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2024-09-25 21:55:08 +0100

    add NvimTree plugin and basic configuration

--- init.lua
+++ init.lua
@@ -86,12 +86,21 @@ P.S. You can delete this when you're done too. It's your config now! :)
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
+
+-- NOTE: NvimTree setup requiring disable of netrw and setting 24 bit colour (optional)
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+--
+--
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -929,7 +938,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
