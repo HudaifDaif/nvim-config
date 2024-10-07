1c4b1dd3b497942cb325b96f49b8485d243c337a
HudaifDaif <daiftem@gmail.com>
2024-10-07 21:43:33 +0100

    update autosession and nvim tree configuration

--- lua/plugins/autosession.lua
+++ lua/plugins/autosession.lua
@@ -14,7 +14,7 @@ return {
         root_dir = vim.fn.stdpath 'data' .. '/sessions/', -- Root dir where sessions will be stored
         auto_save = true, -- Enables/disables auto saving session on exit
         auto_restore = true, -- Enables/disables auto restoring session on start
-        auto_create = true, -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
+        auto_create = false, -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
         allowed_dirs = nil, -- Allow session restore/create in certain directories
         auto_restore_last_session = false, -- On startup, loads the last saved session if session for cwd does not exist
         use_git_branch = false, -- Include git branch name in session name
@@ -24,7 +24,7 @@ return {
         args_allow_single_directory = true, -- Follow normal sesion save/load logic if launched with a single directory as the only argument
         args_allow_files_auto_save = false, -- Allow saving a session even when launched with a file argument (or multiple files/dirs). It does not load any existing session first. While you can just set this to true, you probably want to set it to a function that decides when to save a session when launched with file args. See documentation for more detail
         continue_restore_on_error = true, -- Keep loading the session even if there's an error
-        cwd_change_handling = false, -- Follow cwd changes, saving a session before change and restoring after
+        cwd_change_handling = true, -- Follow cwd changes, saving a session before change and restoring after
         log_level = 'error', -- Sets the log level of the plugin (debug, info, warn, error).
 
         session_lens = {
